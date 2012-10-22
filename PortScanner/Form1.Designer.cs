faa7879076c9e4f0ceee8edcb42b3abe5307558f
BUKT0P <viktor.prokashev@gmail.com>
2012-10-22 21:43:10 +0400

    Work version

---
+++ PortScanner/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace PortScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сканировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "localhost";
+            this.textBox1.WordWrap = false;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(2, 24);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(361, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 172);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Column1.HeaderText = "Открытые порты";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 560;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(67, 24);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(61, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Адрес назначения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Диапазон портов";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(3, 24);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ожидание";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(110, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(131, 47);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.numericUpDown3);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(244, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(115, 47);
+            this.panel2.TabIndex = 11;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(61, 26);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Авто";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 224);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.progressBar1.Size = new System.Drawing.Size(445, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 247);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(461, 285);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(461, 285);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Port scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PortScanner/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace PortScanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, string> dict = new Dictionary<int, string>();//Словарь портов
+        class destination //Класс содержащий параметры сканирования
+        {
+            public string ip = "localhost";
+            public int port = 80;
+            public int wait = 20;
+            public destination(string _ip, int _port, int _wait)
+            {
+                ip = _ip;
+                port = _port;
+                wait = _wait;
+            }
+        }
+        static int Scan(object _in)//Функция сканирования
+        {
+            destination where = (destination)_in;
+            Socket socketToServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IAsyncResult resultOfConnect = socketToServer.BeginConnect(where.ip, where.port, null, socketToServer);
+            if (resultOfConnect.AsyncWaitHandle.WaitOne(where.wait) == true)
+            {
+                socketToServer.Close();
+                return where.port;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private void button1_Click(object destinationer, EventArgs e)//Запуск сканирования
+        {
+            Ping pingSender = new Ping();
+            try
+            {
+                PingReply res = pingSender.Send(textBox1.Text);
+                if(res.Status==IPStatus.Success)
+                {
+                    dataGridView1.Rows.Clear();//Очистка таблицы
+                    backgroundWorker1.RunWorkerAsync();//Запуск сканирования
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    textBox1.ReadOnly = true;
+                    this.Text = "Port scanner (идёт сканирование)";
+                    numericUpDown1.Enabled = false;
+                    numericUpDown2.Enabled = false;
+                    numericUpDown3.Enabled = false;
+                    checkBox1.Enabled = false;
+                }
+            }
+            catch (System.Net.NetworkInformation.PingException)
+            {
+                MessageBox.Show("Недостижим адрес обращения", "Ошибка");
+            }
+        }
+        private void button2_Click(object destinationer, EventArgs e)//Прерывание сканирования
+        {
+            backgroundWorker1.CancelAsync();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox1.ReadOnly = false;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            if (!checkBox1.Checked)
+            {
+                numericUpDown3.Enabled = true;
+            }
+            this.Text = "Port scanner";
+            checkBox1.Enabled = true;
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)//Поток обработки сканирования
+        {
+            int start = decimal.ToInt32(numericUpDown1.Value);
+            int end = decimal.ToInt32(numericUpDown2.Value);
+            int wait = decimal.ToInt32(numericUpDown3.Value);
+            if (checkBox1.Checked)//Автоматическая настройка ожидания
+            {
+                Ping pingSender = new Ping();
+                PingReply res = pingSender.Send(textBox1.Text);
+                wait = (int)res.RoundtripTime + 10;
+                Console.WriteLine(wait.ToString());
+            }
+            for (int i = start; i <= end; i++)//Цикл сканирования
+            {
+                if (this.backgroundWorker1.CancellationPending != true)
+                {
+                    Console.WriteLine(i.ToString());
+                    int result = Scan(new destination(textBox1.Text, i, wait));
+                    if (result != 0)
+                    {
+                        this.backgroundWorker1.ReportProgress(i + 100000);
+                    }
+                    else
+                    {
+                        this.backgroundWorker1.ReportProgress(i);
+                    }
+                }
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)//Окончание сканирования
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox1.ReadOnly = false;
+            this.Text = "Port scanner";
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            if (!checkBox1.Checked)
+            {
+                numericUpDown3.Enabled = true;
+            }
+            checkBox1.Enabled = true;
+        }
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)//Добавление разультата в таблицу
+        {
+            int progress = e.ProgressPercentage;
+            if (e.ProgressPercentage > 65536)//Порт открыт
+            {
+                progress = progress - 100000;
+                string name;
+                try
+                {
+                    name = dict[progress];//Поиск в словаре
+                }
+                catch
+                {
+                    name = "Без названия";
+                }
+                int k = dataGridView1.Rows.Add(progress.ToString() + " - " + name);
+                dataGridView1[0, k].Style.ForeColor = Color.Red;
+            }
+            int start = decimal.ToInt32(numericUpDown1.Value);
+            int end = decimal.ToInt32(numericUpDown2.Value);
+            progressBar1.Value = (progress - start) * 100 / (end - start);//Правка полоски выполнения
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)//Выбор ожидания
+        {
+            if (checkBox1.Checked)
+            {
+                numericUpDown3.Enabled = false;
+            }
+            else
+            {
+                numericUpDown3.Enabled = true;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)//Загрузка словаря портов
+        {
+            if (File.Exists(Application.StartupPath + "\\ports.txt"))
+            {
+                StreamReader loadSettings = new StreamReader(Application.StartupPath + "\\ports.txt", false);
+                string buffer = loadSettings.ReadLine();
+                int end = int.Parse(buffer);
+                for (int i = 1; i <= end; i++)//Загрузка из файла
+                {
+                    buffer = loadSettings.ReadLine();
+                    string[] ink = new string[2];
+                    ink = buffer.Split('	');
+                    dict.Add(int.Parse(ink[0]), ink[1]);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Отсутствует список портов", "Ошибка");
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)//Запуск по Enter
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            } 
+        }
+    }
+}
